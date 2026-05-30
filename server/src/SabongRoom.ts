1f9095ce527c3dc2e4bc8980920c1e4114e241d3
Kim Cyriel S. Avillanosa <km.avillanosa@cabinetsbycomputer.com>
2026-05-30 13:29:01 +0800

    initial

--- server/src/SabongRoom.ts
+++ server/src/SabongRoom.ts
@@ -248,7 +248,7 @@ export class SabongRoom extends Room<SabongState> {
       const px = ox - t * ax, pz = oz - t * az;
       const perp = Math.hypot(px, pz);
       if (perp > HIT_RADIUS) return;
-      if (!best || t < best.t) best = { other: other as Bird, t };
+      if (!best || t < best.t) best = { other: other as Bird, t } as { other: Bird; t: number };
     });
 
     const endT = best ? best.t : w.range;
